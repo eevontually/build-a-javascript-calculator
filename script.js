ab06aafa22f645cdf5cd172fa65467d20066cbbd
Eevon <109541288+eevontually@users.noreply.github.com>
2023-05-08 13:16:05 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+let trailingResult = 0;
+let operationOptions = ['add','subtract', 'multiply','divide'];
+let workingOperation = "";
+
+function updateDisplay(input) {
+ let display = document.getElementById("display");
+
+
+if (display.innerHTML === "0" && operationOptions.indexOf(input) === -1) {
+  if (input === "decimal") {
+  display.innerHTML = "0.";
+} else if (input === "negative-value") {
+  if(display.innerHTML.indexOf("-1") === -1) {
+    display.innerHTML = "-" + display.innerHTML
+} else if (display.innerHTML.indexOf("-1" > -1)){
+  display.innerHTML = display.innerHTML.slice(1, display.innerHTML.length);
+}
+   
+} else {
+   display.innerHTML = input;
+}
+ 
+} else if (operationOptions.indexOf(input) >= 0) {
+  //console.log("we're dealing with an operation");
+  
+  if (trailingResult === display.innerHTML) {
+    //operand was selected twice
+      workingOperation = input;
+} else if (workingOperation === "") {
+    //dealing without an operand
+    workingOperation = input;
+    trailingResult = display.innerHTML;
+    display.innerHTML = 0; 
+} else { 
+   //dealing with a set operand
+  console.log(display.innerHTML, "dealing with a set operand");
+    trailingResult = calculate(trailingResult, display.innerHTML, workingOperation);
+    display.innerHTML = 0;
+    workingOperation = input; 
+}
+  
+} else if (input === "equals") {
+  display.innerHTML = calculate(trailingResult, display.innerHTML, workingOperation);
+  trailingResult = 0;
+  workingOperation = "";
+} else if (input === "decimal"){
+  //console.log("decimal clicked");
+  if (display.innerHTML.indexOf(".") === -1){
+    display.innerHTML += ".";
+} 
+  //console.log("Decimal skipped because decimal already in number");
+} else if (input === "negative-value"){
+  console.log("negative-value selected")
+  if(display.innerHTML.indexOf("-1") === -1) {
+    display.innerHTML = "-" + display.innerHTML
+} else if (display.innerHTML.indexOf("-1" > -1)){
+  display.innerHTML = display.innerHTML.slice(1, display.innerHTML.length);
+}
+  
+ } else {
+  display.innerHTML += input;
+ }
+ console.log(trailingResult, "<= trailingResult", display.innerHTML, "<== display.innerHTML", workingOperation, "<== workingOperation");
+}
+
+
+function clearDisplay() {
+  let display = document.getElementById("display");
+  trailingResult = 0;
+ display.innerHTML = 0;
+}
+
+function calculate(firstNumber, secondNumber, operation){
+  let result;
+  firstNumber = parseFloat(firstNumber);
+  secondNumber = parseFloat(secondNumber);
+  switch(operation) {
+  case "add":
+    result = firstNumber + secondNumber;
+    break;
+  case "subtract":
+    result = firstNumber - secondNumber;
+    break;
+  case "multiply":
+    result = firstNumber * secondNumber;
+    break;
+   case "divide":
+    result = firstNumber / secondNumber;
+  }
+  return result.toString();
+}
